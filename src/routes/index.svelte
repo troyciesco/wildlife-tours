8aaef95f92b704545f5cd3851b11ebba0b2e8746
tmciesco <tmciesco@gmail.com>
2022-08-01 11:05:46 -0400

    meta updates

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,8 +4,28 @@
 </script>
 
 <svelte:head>
-	<title>Lord Rhubarb's Wildlife Tours</title>
 	<meta name="description" content="Lord Rhubarb's Moose on the Loose Wildlife Tours" />
+	<title>Lord Rhubarb's Moose on the Loose Wildlife Tours</title>
+	<meta name="title" content="Lord Rhubarb's Moose on the Loose Wildlife Tours" />
+	<meta name="description" content="The greatest wildlife tour on earth led by the greatest moose in the galaxy." />
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://wildlife-tours.vercel.app" />
+	<meta property="og:title" content="Lord Rhubarb's Moose on the Loose Wildlife Tours" />
+	<meta property="og:description" content="The greatest wildlife tour on earth led by the greatest moose in the galaxy." />
+	<meta
+		property="og:image"
+		content="https://images.unsplash.com/photo-1602391950852-88bf9be72b24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1600&q=80" />
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content="https://wildlife-tours.vercel.app" />
+	<meta property="twitter:title" content="Lord Rhubarb's Moose on the Loose Wildlife Tours" />
+	<meta property="twitter:description" content="The greatest wildlife tour on earth led by the greatest moose in the galaxy." />
+	<meta
+		property="twitter:image"
+		content="https://images.unsplash.com/photo-1602391950852-88bf9be72b24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1600&q=80" />
 </svelte:head>
 
 <main>
